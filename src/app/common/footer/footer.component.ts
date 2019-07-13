0914ff4d31080d4dd50c722506d0054c6cf879dd
hlakaza <nhlalucky9@gmail.com>
2019-07-13 19:20:31 +0200

    refactor(footer): added dynamic content

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { content } from '../../shared/content';
 
 @Component({
   selector: 'app-footer',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  footer = content.footer;
   constructor() { }
 
   ngOnInit() {
--- src/app/shared/content.ts
+++ src/app/shared/content.ts
@@ -1,41 +1,55 @@
 export const content = {
-  banner: {
-    line1: 'WE ARE',
-    line2: 'DIGITAL',
-    line3: 'DESIGN & DEVELOPMENT SERVICE PROVIDER.'
-  },
-  services: [
-    {
-      icon: '',
-      header: 'Responsive Design',
-      paragraph: `We create user-friendly navigation and responsive content layout. Your Website will show on
-      different devices. You will gain more traffic with websites optimized
-      for desktop, tablet and mobile viewing.`
-    },
-    {
-      icon: '',
-      header: 'Web Development',
-      paragraph: `We provide a fully transparent development process backed with Seamless Communication & Understanding, We give life to
-      your landing pages and web applications, We  Maintain, Support & make Improvements`
-    },
-    {
-      icon: '',
-      header: 'Social Integration',
-      paragraph: `Integrating your social network profiles with your website to create a seamless customer experience.
+         banner: {
+           line1: 'WE ARE',
+           line2: 'DIGITAL',
+           line3: 'DESIGN & DEVELOPMENT SERVICE PROVIDER.'
+         },
+         services: [
+           {
+             icon: '',
+             header: 'Responsive Design',
+             paragraph: `We create user-friendly navigation and responsive content layout. Your Website will show on
+            different devices. You will gain more traffic with websites optimized
+            for desktop, tablet and mobile viewing.`
+           },
+           {
+             icon: '',
+             header: 'Web Development',
+             paragraph: `We provide a fully transparent development process backed with Seamless Communication &
+             Understanding, We give life to your landing pages and web applications, We  Maintain, Support & make Improvements`
+           },
+           {
+             icon: '',
+             header: 'Social Integration',
+             paragraph: `Integrating your social network profiles with your website to create a seamless customer experience.
       Social media will reduce your overall marketing costs and gain the competitive advantage.`
-    },
-    {
-      icon: '',
-      header: 'SEO',
-      paragraph: `We will improve the number of high-quality visits to your website.
+           },
+           {
+             icon: '',
+             header: 'SEO',
+             paragraph: `We will improve the number of high-quality visits to your website.
       The more visitors you have to your website the more leads and essentially sales you will generate.`
-    },
-    {
-      icon: '',
-      header: 'Digital Advertising',
-      paragraph: `We will deliver performing social media campaigns for your business.
+           },
+           {
+             icon: '',
+             header: 'Digital Advertising',
+             paragraph: `We will deliver performing social media campaigns for your business.
       We will position your product at the top through social media marketing.
       Our campaigns deliver engaging content and increase followers`
-    }
-  ]
-};
+           }
+         ],
+         footer: {
+           qoute: {
+             author: 'Henry Ford',
+             qoute: `A business absolutely devoted to service will have one worry about profits. They will be embarrassingly large`
+           },
+           block1: {
+             header: 'Newsletter',
+             paragraph:
+               'Sign up and keep up-to-date with exclusive products, discounts and promotions.'
+           },
+           block2: {
+             header: 'Instagram'
+           }
+         }
+       };
